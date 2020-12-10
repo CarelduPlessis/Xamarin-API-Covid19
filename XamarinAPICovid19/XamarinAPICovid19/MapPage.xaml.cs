a02c548bf0d6b217c1c8756dc8b416cc680bf6fd
cdup <cdup@MSI>
2020-12-11 10:34:50 +1300

    added Comments

--- XamarinAPICovid19/XamarinAPICovid19/MapPage.xaml.cs
+++ XamarinAPICovid19/XamarinAPICovid19/MapPage.xaml.cs
@@ -11,8 +11,8 @@ namespace XamarinAPICovid19
     
     public partial class MapPage : ContentPage
     {
-        Position position;
-        Xamarin.Forms.Maps.Map map;
+        Position position; // store the position of the Country
+        Xamarin.Forms.Maps.Map map; 
         Entry country;
         Button findLocationBTN;
 
@@ -28,11 +28,11 @@ namespace XamarinAPICovid19
             InitializeComponent();
             CAPI = new CovidAPI();
             map = new Xamarin.Forms.Maps.Map();
-            map.HasZoomEnabled = false;
-            map.HasScrollEnabled = false;
-            map.Pins.Clear();
+            map.HasZoomEnabled = false; //disable zoom on the map
+            map.HasScrollEnabled = false;//disable Scroll on the map
+            map.Pins.Clear(); // clear all the pins form the map
 
-            mainStackLayout = new StackLayout();
+            mainStackLayout = new StackLayout(); // the main layout
 
             country = new Entry
             {
@@ -43,13 +43,14 @@ namespace XamarinAPICovid19
             {
                 Text = "Check Covid-19 in this Country"
             };
-            findLocationBTN.Clicked += getLoction;
+            findLocationBTN.Clicked += getLoction; // get the country location
 
+            // add all the elements to the main layout
             mainStackLayout.Children.Add(map);
             mainStackLayout.Children.Add(country);
             mainStackLayout.Children.Add(findLocationBTN);
 
-            Content = mainStackLayout;
+            Content = mainStackLayout; // add main layout to the page
             
            // Thread.Sleep(2000);
            // _ = FindTheLocation("", "", "New Zealand");
@@ -69,29 +70,35 @@ namespace XamarinAPICovid19
                 //Title: Using addresses, maps and geocoordinates in your Xamarin Forms apps 
                 //URL: https://mallibone.com/post/xamarin-maps-addresses-geocoords
 
+                // get location from the address 
                 var locations = await Geocoding.GetLocationsAsync($"{street},{city},{country}");
 
-                var location = locations?.FirstOrDefault();
+                var location = locations?.FirstOrDefault(); // get the first result in the list
 
-                if (location == null)
+                if (location == null) // check if location has been found / location is not empty
                 {
                     await DisplayAlert("Invaled Country", "Can't find Country: try using capital letters like New Zealand or duoble check spelling", "OK");
                     return;
                 }
 
                 position = new Position(location.Latitude, location.Longitude);
+
+                // start the task of geting the data from the Covid19 API
                 var placeholderDeaths = Deaths(country);
                 var placeholderRecovered = Recovered(country);
                 var placeholderConfirmed = Confirmed(country);
 
+                // wait untell all of them have completed their task
                 await Task.WhenAll(placeholderDeaths, placeholderRecovered, placeholderConfirmed);
 
+                // get the result of the task and store it in a global variable
                 deaths = placeholderDeaths.Result;
                 recovered = placeholderRecovered.Result;
                 confirmed = placeholderConfirmed.Result;
 
+                //create the pins on the map 
                 CreatePins(country);
-                MoveTo();
+                MoveTo(); // move the Google API Map to the position of the country
             }
             catch (Exception ex)
             {
@@ -100,29 +107,34 @@ namespace XamarinAPICovid19
             }
         }
 
+        //Create Pin on Google API Map
         public void CreatePins(string country)
         {
-            Pin pin = new Pin()
+            Pin pin = new Pin() // fill the pin with information
             {
                 Label = "Deaths: " + deaths + " " + "Recovered: " + recovered + " " + "Confirmed: " + confirmed,
                 Address = country,
                 Type = PinType.Place,
-                Position = new Position(position.Latitude, position.Longitude)
+                Position = new Position(position.Latitude, position.Longitude) // tell pin where on the map to be placed
             };
-            map.Pins.Add(pin);
+            map.Pins.Add(pin); // add pin to the map
         }
 
+        //Move the Google API Map to a position on the map
         public void MoveTo()
         {
             if (map.VisibleRegion != null)
             {
-                Console.WriteLine("***************************************");
-                Console.WriteLine("Hello");
-                Console.WriteLine("***************************************");
                 map.MoveToRegion(new MapSpan(position, position.Latitude, position.Longitude));
             }
         }
 
+        /*
+            Get data form the Covid19 API:
+            
+            Deaths, Recovered and Confirmed
+         
+         */
         public async Task<int> Deaths(string country)
         {
             var a = await CAPI.ReturnDeaths(country);
